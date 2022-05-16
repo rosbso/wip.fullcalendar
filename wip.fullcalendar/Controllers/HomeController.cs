46d04c88d960c23a4ae69edef2a36dd954507433
Robson <robson.feitosa@ufmt.br>
2022-05-15 21:28:44 -0400

    primeiro commit

---
+++ wip.fullcalendar/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+using wip.fullcalendar.Models;
+
+namespace wip.fullcalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Calendario()
+        {
+            return View();
+        }
+
+        public JsonResult GetEvents()
+        {
+            //var viewModel = new HorarioViewModel();
+            //var events = new List<EventViewModel>();
+
+            string path = "Controllers/data.json";
+            string readText = System.IO.File.ReadAllText(path);
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var events = JsonSerializer.Deserialize<List<HorarioViewModel>>(readText, options);
+
+
+            return Json(events.ToArray());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wip.fullcalendar/Models/HorarioViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace wip.fullcalendar.Models
+{
+    public class HorarioViewModel
+    {
+        public string Id { get; set; }
+        public bool AllDay { get; set; }
+        public string Title { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string StartRecur { get; set; }
+        public string EndRecur { get; set; }
+        public int[] DaysOfWeek { get; set; }
+
+    }
+}
